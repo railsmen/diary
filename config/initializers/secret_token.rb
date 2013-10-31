e10b9ed87a2cdf66db46bac3bd2ed610d101c349
visitvinoth <visitvinoth@gmail.com>
2013-10-31 06:23:51 -0400

    commit secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diary::Application.config.secret_token = '941444465c540efef6a9bbaef7805e97cc5c878b8263f9004886baf7ad6f6a1f6eeb699e041f256a6410cb8f599b9065a79447e3ddd38beda0e7bb1d6a87c400'
